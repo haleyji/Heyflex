4e6a12628edeba816f38eac82d6e2c408337d74d
haleyji <hyeyounghaleyji@gmail.com>
2020-12-15 15:12:32 +0900

    change repository name

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-const Movie = ({id, year, title, summary, poster}) => {
+const Movie = ({id, year, title, summary, poster, genres}) => {
     return (
         <div className="movie">
+            
             <img src={poster} alt={title} className="movie_poster"/>
             <div className="movie_data">
                 <h3 className="movie_title">{title}</h3>
                 <h5 className="movie_year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres_genre">{genre}</li>
+                    ))}
+                </ul>
                 <p className="movie_summary">{summary}</p>
             </div>
         </div>
@@ -19,7 +26,8 @@ Movie.propTypes = {
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default Movie;
